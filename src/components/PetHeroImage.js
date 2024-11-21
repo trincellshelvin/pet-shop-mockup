e707b88493c77979471eca014c630ed3be67a4b3
trincellshelvin <trincellshelvin@gmail.com>
2024-11-20 20:33:17 -0600

    completed hero image

--- src/components/PetHeroImage.js
+++ src/components/PetHeroImage.js
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 // TODO: in storybook, create a story for this component
 // update the value for src in the story
 // The image is located in public/img/hero-dog.png
-export default function PetHeroImage (props) {
+export default function PetHeroImage(props) {
+  const { src } = props;
   return (
-    <div>
-      TODO: Finish this component based on the design in public/mockups/hero-image.png
+    <div className="relative">
+      <div className="absolute bottom-0 left-0 w-full h-4/5 bg-gradient-to-b from-rose-400 to-rose-600 rounded-2xl z-0"></div>
+      <img
+        src={src}
+        alt="dog hero image"
+        className="relative z-10 w-full object-cover"
+      />
     </div>
   );
-};
+}
 
 PetHeroImage.propTypes = {
   src: PropTypes.string.isRequired,
---
+++ src/components/PetHeroImage.stories.jsx
@@ -0,0 +1,9 @@
+import PetHeroImage from './PetHeroImage';
+
+const meta = {
+  component: PetHeroImage,
+};
+
+export default meta;
+
+export const Default = {};
